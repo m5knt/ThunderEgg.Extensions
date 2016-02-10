2d28877883077529a2c6bede8fa3e85a4c653acf
m5knt <m5knt@inetsbb.com>
2016-02-11 01:27:50 +0900

    変換回り追加

---
+++ src/ConvertibleExtension.cs
@@ -0,0 +1,53 @@
+﻿/**
+ * @brief StringBuffer回り
+ */
+
+using System;
+using System.Text;
+
+namespace ThunderEgg.Extentions {
+
+    /// <summary>IConvertible回り</summary>
+    public static class ConvertibleExtension {
+
+        /// <summary>型変換します</summary>
+        public static T To<T>(this IConvertible value) {
+            var ty = typeof(T);
+            var u = Nullable.GetUnderlyingType(ty);
+            if (u != null) {
+                try {
+                    return (T)Convert.ChangeType(value, u);
+                }
+                catch {
+                    return default(T);
+                }
+            }
+            else {
+                return (T)Convert.ChangeType(value, ty);
+            }
+        }
+
+        /// <summary>型変換します</summary>
+        public static T To<T>(this IConvertible value, T or_default) {
+            var ty = typeof(T);
+            var u = Nullable.GetUnderlyingType(ty);
+            if (u != null) {
+                try {
+                    return (T)Convert.ChangeType(value, ty);
+                }
+                catch {
+                    return default(T);
+                }
+            }
+            else {
+                try {
+                    return (T)Convert.ChangeType(value, ty);
+                }
+                catch {
+                    return or_default;
+                }
+            }
+        }
+    }
+}
+
---
+++ test/ConvertibleTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using ThunderEgg.Extentions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace test {
+    [TestClass]
+    public class ConvertibleTest {
+
+        [TestMethod]
+        public void To() {
+
+            Assert.AreEqual(1, "1".To<int>());
+            Assert.AreEqual(1, 1.1.To<int>());
+
+            Assert.AreEqual(1, "1".To<int?>());
+            Assert.AreEqual(null, ((string)null).To<int?>());
+            Assert.AreEqual(null, "".To<int?>());
+            Assert.AreEqual(null, "a".To<int?>());
+
+            Assert.AreEqual(1, "a".To<int>(1));
+
+            try { "".To<int>(); Assert.Fail(); } catch (Exception e) { }
+            try { ((string)null).To<int>(); Assert.Fail(); } catch (Exception e) { }
+        }
+    }
+}
