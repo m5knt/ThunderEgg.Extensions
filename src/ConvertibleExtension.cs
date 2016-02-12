ce1e551ff2003a96413ffd4a0152315ceb0d6821
m5knt <m5knt@inetsbb.com>
2016-02-12 22:21:43 +0900

    失敗時は単純にdefaultを返すように変更

--- src/ConvertibleExtension.cs
+++ src/ConvertibleExtension.cs
@@ -10,42 +10,30 @@ namespace ThunderEgg.Extentions {
     /// <summary>IConvertible回り</summary>
     public static class ConvertibleExtension {
 
-        /// <summary>型変換します</summary>
-        public static T To<T>(this IConvertible value) {
-            var ty = typeof(T);
-            var u = Nullable.GetUnderlyingType(ty);
-            if (u != null) {
-                try {
-                    return (T)Convert.ChangeType(value, u);
-                }
-                catch {
-                    return default(T);
-                }
+        /// <summary>値の型を変換します</summary>
+        /// <returns>型を変換した値</returns>
+        public static T To<T>(this IConvertible self) {
+            var t = typeof(T);
+            t = Nullable.GetUnderlyingType(t) ?? t;
+            try {
+                return (T)Convert.ChangeType(self, t);
             }
-            else {
-                return (T)Convert.ChangeType(value, ty);
+            catch {
+                return default(T);
             }
         }
 
-        /// <summary>型変換します</summary>
-        public static T To<T>(this IConvertible value, T or_default) {
-            var ty = typeof(T);
-            var u = Nullable.GetUnderlyingType(ty);
-            if (u != null) {
-                try {
-                    return (T)Convert.ChangeType(value, ty);
-                }
-                catch {
-                    return default(T);
-                }
+        /// <summary>値の型を変換します</summary>
+        /// <param name="default">失敗時の値</param>
+        /// <returns>型を変換した値</returns>
+        public static T To<T>(this IConvertible self, T @default) {
+            var t = typeof(T);
+            t = Nullable.GetUnderlyingType(t) ?? t;
+            try {
+                return (T)Convert.ChangeType(self, t);
             }
-            else {
-                try {
-                    return (T)Convert.ChangeType(value, ty);
-                }
-                catch {
-                    return or_default;
-                }
+            catch {
+                return @default;
             }
         }
     }
