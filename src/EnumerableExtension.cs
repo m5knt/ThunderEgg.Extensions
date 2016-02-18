6bd5ed7120ffbc0ff876513de1dad2380b1066a3
m5knt <m5knt@inetsbb.com>
2016-02-19 02:41:44 +0900

    Do, Range, Repeat 追加

---
+++ src/EnumerableExtension.cs
@@ -0,0 +1,80 @@
+﻿/**
+ * @brief StringBuffer回り
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Linq;
+
+namespace ThunderEgg.Extentions {
+
+    public static partial class A {
+
+        /// <summary>値を列挙します</summary>
+        public static IEnumerable<int> Range(int end) {
+            for (var i = 0; i < end; ++i) yield return i;
+        }
+
+        /// <summary>値を列挙します</summary>
+        public static IEnumerable<int> Range(int start, int end) {
+            if (start < end) {
+                for (var i = start; i < end; ++i) yield return i;
+            }
+            else {
+                for (var i = start; i > end; --i) yield return i;
+            }
+        }
+
+        /// <summary>値を列挙します</summary>
+        public static IEnumerable<int> Range(int start, int end, int step) {
+            if (start < end) {
+                for (var i = start; i < end; i += step) yield return i;
+            }
+            else {
+                for (var i = start; i > end; i += step) yield return i;
+            }
+        }
+    }
+
+    /// <summary>IEnumerable回り</summary>
+    public static class EnumerableExtension {
+
+        /// <summary>値を指定回数繰り返します</summary>
+        public static IEnumerable<T> Repeat<T>(this T self, int limit) {
+            for (var i = 0; i < limit; ++i) yield return self;
+        }
+
+        /// <summary>値毎に処理をします</summary>
+        public static void Do<T>(this IEnumerable<T> e, Action<T> func) {
+            foreach (var t in e) func(t);
+        }
+
+        /// <summary>値毎に処理をします</summary>
+        public static void Do<T>(this IEnumerable<T> e, Action<T, int> func) {
+            var i = 0;
+            foreach (var t in e) func(t, i++);
+        }
+
+        /// <summary>値毎に処理をします</summary>
+        public static IEnumerable<E> Do<T, E>(this IEnumerable<T> e, Func<T, E> func) {
+            foreach (var t in e) yield return func(t);
+        }
+
+        /// <summary>値毎に処理をします</summary>
+        public static IEnumerable<E> Do<T, E>(this IEnumerable<T> e, Func<T, int, E> func) {
+            var i = 0;
+            foreach (var t in e) yield return func(t, i++);
+        }
+
+#if false
+        /// <summary>値を指定回数繰り返します</summary>
+        public static IEnumerable<T> Times<T>(this int self, Func<int, T> func) {
+            for (var i = 0; i < self; ++i) yield return func(i);
+        }
+#endif
+
+    }
+}
+
---
+++ test/EnumerableTest.cs
@@ -0,0 +1,57 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using ThunderEgg.Extentions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace test {
+    [TestClass]
+    public class CollectionTest {
+
+        [TestMethod]
+        public void Times() {
+            //            Assert.IsTrue(new[] { 0, 0, 0 }.SequenceEqual(3.Times(() => 0)));
+            //            Assert.IsTrue(new[] { 0, 1, 2 }.SequenceEqual(3.Times(i => i)));
+        }
+
+        [TestMethod]
+        public void Do() {
+            List<int> ret;
+            ret = new List<int>();
+            0.Repeat(3).Do(_ => ret.Add(_));
+            Assert.IsTrue(new[] { 0, 0, 0 }.SequenceEqual(ret));
+            ret = new List<int>();
+            0.Repeat(3).Do((_, i) => ret.Add(i));
+            Assert.IsTrue(new[] { 0, 1, 2 }.SequenceEqual(ret));
+            ret = 0.Repeat(3).Do(_ => _).ToList();
+            Assert.IsTrue(new[] { 0, 0, 0 }.SequenceEqual(ret));
+            ret = 0.Repeat(3).Do((_, i) => i).ToList();
+            Assert.IsTrue(new[] { 0, 1, 2 }.SequenceEqual(ret));
+        }
+
+        [TestMethod]
+        public void Range() {
+            List<int> ret;
+            ret = new List<int>();
+            A.Range(3).Do(_ => ret.Add(_));
+            Assert.IsTrue(new[] { 0, 1, 2 }.SequenceEqual(ret));
+            ret = new List<int>();
+            A.Range(0, 3).Do(_ => ret.Add(_));
+            Assert.IsTrue(new[] { 0, 1, 2 }.SequenceEqual(ret));
+            ret = new List<int>();
+            A.Range(0, 5, 2).Do(_ => ret.Add(_));
+            Assert.IsTrue(new[] { 0, 2, 4 }.SequenceEqual(ret));
+            ret = new List<int>();
+            A.Range(0, -3, -1).Do(_ => ret.Add(_));
+            Assert.IsTrue(new[] { 0, -1, -2 }.SequenceEqual(ret));
+            A.Range(4).Select(_ => _);
+        }
+
+        [TestMethod]
+        public void Repeat() {
+            Assert.IsTrue(new[] { 0, 0, 0 }.SequenceEqual(0.Repeat(3)));
+        }
+    }
+}
