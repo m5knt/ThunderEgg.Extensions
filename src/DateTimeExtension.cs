91e84119c55a811260554f69dbb97fe8abac4ba2
m5knt <m5knt@inetsbb.com>
2016-02-19 22:42:56 +0900

    整理

---
+++ src/DateTimeExtension.cs
@@ -0,0 +1,16 @@
+﻿/**
+ * @file
+ * @brief DateTimeの拡張メソッド関係
+ * @author Yukio KANEDA
+ */
+
+using System;
+using System.Runtime.CompilerServices;
+
+namespace ThunderEgg.Extentions {
+
+    public static partial class A {
+        static DateTime UnixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+    }
+}
+
