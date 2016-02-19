9a2e1b239c2c6c5ca11e1c4f6268889f6e065e03
m5knt <m5knt@inetsbb.com>
2016-02-19 22:42:31 +0900

    timespan系追加

---
+++ src/TimeSpanExtension.cs
@@ -0,0 +1,41 @@
+﻿/**
+ * @file
+ * @brief TimeSpanの拡張メソッド関係
+ * @author Yukio KANEDA
+ */
+
+using System;
+using System.Runtime.CompilerServices;
+
+namespace ThunderEgg.Extentions {
+
+    /// <summary>TimeSpanの拡張メソッド関係</summary>
+    public static partial class TimeSpanExtension {
+
+        /// <summary>時間を返します</summary>
+        public static TimeSpan Days(this int n) {
+            return new TimeSpan(n, 0, 0, 0);
+        }
+
+        /// <summary>時間を返します</summary>
+        public static TimeSpan Hours(this int n) {
+            return new TimeSpan(n, 0, 0);
+        }
+
+        /// <summary>時間を返します</summary>
+        public static TimeSpan Minutes(this int n) {
+            return new TimeSpan(0, n, 0);
+        }
+
+        /// <summary>時間を返します</summary>
+        public static TimeSpan Seconds(this int n) {
+            return new TimeSpan(0, 0, n);
+        }
+
+        /// <summary>時間を返します</summary>
+        public static TimeSpan MilliSeconds(this int n) {
+            return new TimeSpan(0, 0, 0, 0, n);
+        }
+    }
+}
+
