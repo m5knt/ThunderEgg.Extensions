a339b575771d77af09a42b923c3d10a0d9f2fe0f
m5knt <m5knt@inetsbb.com>
2016-02-28 14:50:40 +0900

    初日晦日取得関数追加

--- src/DateTimeExtension.cs
+++ src/DateTimeExtension.cs
@@ -10,7 +10,23 @@ using System.Runtime.CompilerServices;
 namespace ThunderEgg.Extentions {
 
     public static partial class A {
+
+        /// <summary>Unixエポック</summary>
         static DateTime UnixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+
+        /// <summary>月の初日</summary>
+        public static DateTime FirstDayOfMonth(this DateTime self) {
+            var t = 1 - self.Day;
+            return t == 0 ? self : self.AddDays(t);
+        }
+
+        /// <summary>月の晦日</summary>
+        public static DateTime LastDayOfMonth(this DateTime self) {
+            var t = DateTime.DaysInMonth(self.Year, self.Month) - self.Day;
+            return t == 0 ? self : self.AddDays(t);
+        }
+
+        public static DateTime 
     }
 }
 
---
+++ test/DateTimeTest.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using ThunderEgg.Extentions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace test {
+
+    [TestClass]
+    public class DateTimeTest {
+
+        [TestMethod]
+        public void DateTime() {
+            Assert.AreEqual(new DateTime(2016, 2, 1), new DateTime(2016, 2, 29).FirstDayOfMonth());
+            Assert.AreEqual(new DateTime(2016, 2, 29), new DateTime(2016, 2, 1).LastDayOfMonth());
+        }
+    }
+}
