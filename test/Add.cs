fed8f21cef9aa63f81a5f58e1d668ddcfa1d7975
m5knt <m5knt@inetsbb.com>
2016-02-29 23:44:21 +0900

    addテスト追加

---
+++ test/Add.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using ThunderEgg.Extentions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace test {
+
+    public partial class Misc {
+
+        [TestMethod]
+        public void Add() {
+            Assert.IsTrue(new[] { 0, 1, 2 }.SequenceEqual(new List<int> { 0 }.Add(1, 2)));
+            Assert.IsTrue(new[] { 0, 1, 2 }.SequenceEqual(new List<int> { 0 }.Add(new[] { 1, 2 })));
+        }
+    }
+}
