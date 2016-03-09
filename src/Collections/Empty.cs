49648fcafc36eb6de33219de7e52cdf4b3c10486
m5knt <m5knt@inetsbb.com>
2016-03-09 23:08:04 +0900

    add emptyifnull

---
+++ src/Collections/Empty.cs
@@ -0,0 +1,20 @@
+﻿/**
+ * @file
+ * @brief Repeat操作
+ */
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ThunderEgg.Extentions {
+
+    public static partial class A {
+
+        /// <summary>nullなら空を返す</summary>
+        public static IEnumerable<T> EmptyIfNull<T>(this IEnumerable<T> @this) {
+            return @this ?? Enumerable.Empty<T>();
+        }
+    }
+}
+
