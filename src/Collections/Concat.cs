faf87cb0d00d3830393608090590b28f50e99654
m5knt <m5knt@inetsbb.com>
2016-03-14 00:58:33 +0900

    add concat

---
+++ src/Collections/Concat.cs
@@ -0,0 +1,31 @@
+﻿/**
+ * @file
+ * @brief Repeat操作
+ */
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ThunderEgg.Extentions {
+
+    public static partial class A {
+
+        /// <summary>IEnumerator を連結します</summary>
+        public static IEnumerator //
+            Concat(this IEnumerator @this, IEnumerator follow) //
+        {
+            while (@this.MoveNext()) yield return @this.Current;
+            while (follow.MoveNext()) yield return follow.Current;
+        }
+
+        /// <summary>IEnumerator<T> を連結します</summary>
+        public static IEnumerator<T> //
+            Concat<T>(this IEnumerator<T> @this, IEnumerator<T> follow) //
+        {
+            while (@this.MoveNext()) yield return @this.Current;
+            while (follow.MoveNext()) yield return follow.Current;
+        }
+    }
+}
+
