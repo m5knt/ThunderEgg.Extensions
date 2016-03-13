454a8fa0414d722864399f5181d80d5fc6af8e17
m5knt <m5knt@inetsbb.com>
2016-03-14 00:58:23 +0900

    add cast

---
+++ src/Collections/Cast.cs
@@ -0,0 +1,29 @@
+﻿/**
+ * @file
+ * @brief Repeat操作
+ */
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ThunderEgg.Extentions {
+
+    public static partial class A {
+
+        /// <summary>IEnumeratorをIEnumerable<T>へ変換する</summary>
+        public static IEnumerable<T> Cast<T>(this IEnumerator @this) {
+            while (@this.MoveNext()) {
+                yield return (T)@this.Current;
+            }
+        }
+
+        /// <summary>IEnumeratorをIEnumerable<T>へ変換する</summary>
+        public static IEnumerable<T> Cast<T>(this IEnumerator<T> @this) {
+            while (@this.MoveNext()) {
+                yield return @this.Current;
+            }
+        }
+    }
+}
+
