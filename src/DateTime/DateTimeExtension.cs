c6c135ef5968a3e687b2a6dc36fbb038ef67e2d0
m5knt <m5knt@inetsbb.com>
2016-03-15 00:10:32 +0900

    週の初日・最終日取得関数追加

--- src/DateTime/DateTimeExtension.cs
+++ src/DateTime/DateTimeExtension.cs
@@ -10,21 +10,33 @@ namespace ThunderEgg.Extentions {
 
     public static partial class A {
 
+        /// <summary>うるう年であるか返す</summary>
+        public static bool IsLeapYear(this DateTime @this) {
+            return DateTime.IsLeapYear(@this.Year);
+        }
+
         /// <summary>月の初日</summary>
         public static DateTime FirstDayOfMonth(this DateTime @this) {
             var t = 1 - @this.Day;
             return t == 0 ? @this : @this.AddTicks(t * TimeSpan.TicksPerDay);
         }
 
-        /// <summary>月の晦日</summary>
+        /// <summary>月の最終日</summary>
         public static DateTime LastDayOfMonth(this DateTime @this) {
             var t = DateTime.DaysInMonth(@this.Year, @this.Month) - @this.Day;
             return t == 0 ? @this : @this.AddTicks(t * TimeSpan.TicksPerDay);
         }
 
-        /// <summary>うるう年であるか返す</summary>
-        public static bool IsLeapYear(this DateTime @this) {
-            return DateTime.IsLeapYear(@this.Year);
+        /// <summary>週の初日</summary>
+        public static DateTime FirstDayOfWeek(this DateTime @this) {
+            var t = 0 - (int)@this.DayOfWeek;
+            return t == 0 ? @this : @this.AddTicks(t * TimeSpan.TicksPerDay);
+        }
+
+        /// <summary>週の最終日</summary>
+        public static DateTime LastDayOfWeek(this DateTime @this) {
+            var t = 6 - (int)@this.DayOfWeek;
+            return t == 0 ? @this : @this.AddTicks(t * TimeSpan.TicksPerDay);
         }
     }
 }
