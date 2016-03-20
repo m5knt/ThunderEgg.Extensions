8acdadbb5b1bdbab0c337ae36e5c3caab5dcb039
m5knt <m5knt@inetsbb.com>
2016-03-21 00:29:05 +0900

    add isnull

---
+++ src/ObjectExtension.cs
@@ -0,0 +1,26 @@
+﻿/**
+ * @file
+ * @brief object の拡張メソッド関係
+ */
+
+using System;
+using System.Text;
+
+namespace ThunderEgg.Extentions {
+
+    /// <summary>object の拡張メソッド関係</summary>
+    public static class ObjectExtension {
+
+        /// <summary>null であるかを返す</summary>
+        public static bool IsNull<T>(this T @this) where T : class {
+            return @this == null;
+        }
+
+        /// <summary>null であるかを返す</summary>
+        public static bool IsNull<T>(this T? @this) where T : struct {
+            return !@this.HasValue;
+        }
+
+    }
+}
+
