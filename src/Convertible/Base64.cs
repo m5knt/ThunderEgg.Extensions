4dee48cfa548c3cef22a06968d11a7a1de7018d4
m5knt <m5knt@inetsbb.com>
2016-03-30 00:26:17 +0900

    convertible 整理

---
+++ src/Convertible/Base64.cs
@@ -0,0 +1,24 @@
+﻿/**
+ * @file
+ * @brief BASE64の拡張メソッド
+ */
+
+using System;
+
+namespace ThunderEgg.Extentions {
+
+    public static partial class Base64Extension {
+
+        /// <summary>BASE64文字列からバイト配列へ変換します</summary>
+        public static byte[] Base64ToBytes(this string @this) {
+            return Convert.FromBase64String(@this);
+        }
+
+        /// <summary>バイト配列からBASE64文字列へ変換します</summary>
+        public static string ToBase64(this byte[] @this) {
+            return Convert.ToBase64String(@this);
+        }
+
+    }
+}
+
