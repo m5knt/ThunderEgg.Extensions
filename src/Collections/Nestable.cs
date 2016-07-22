885551f8a504650b1490a28ff8ef0ae6c79a1213
m5knt <m5knt@inetsbb.com>
2016-07-22 09:23:17 +0900

    入れ子可能化追加

---
+++ src/Collections/Nestable.cs
@@ -0,0 +1,30 @@
+﻿/**
+ * @file
+ * @brief Repeat 操作
+ */
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ThunderEgg.Extentions {
+
+    public static partial class A {
+
+        /// <summary>入れ子可能化にします</summary>
+        public static IEnumerator Nestable(this IEnumerator @this) {
+            while (@this.MoveNext()) {
+                var ret = @this.Current;
+                if (ret == null) {
+                    yield return null;
+                }
+                else if (ret is IEnumerator) {
+                    @this = Concat((IEnumerator)ret, @this);
+                }
+                else {
+                    yield return ret;
+                }
+            }
+        }
+    }
+
